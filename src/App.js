f1ae70aa3f8e7170ca8467e19929e9b7418543d3
Eddy Mintela <eddy@Eddys-MacBook-Pro-2.local>
2024-10-27 10:33:07 -0700

    updates case sensitivity for deployment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Video from './pages/Video/Video';
 import Home from './pages/Home/Home';
 
+
 function App() {
   const [sidebar, setSidebar] = useState(true);
   
@@ -15,6 +16,7 @@ function App() {
         sidebar={sidebar}
         setSidebar={setSidebar}
        />
+       
       <Routes>
       <Route path="/" element={<Home sidebar={sidebar}/>} />
         <Route path="/video/:videoId" element={<Video />} />
